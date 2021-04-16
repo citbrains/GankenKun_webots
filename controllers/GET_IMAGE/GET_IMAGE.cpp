23726c1244d049549744eafea9de997830c59c50
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2021-04-16 20:17:25 +0900

    Add C++ code to save the image

---
+++ controllers/GET_IMAGE/GET_IMAGE.cpp
@@ -0,0 +1,60 @@
+// File:          GET_IMAGE.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+#include <webots/Robot.hpp>
+#include <webots/Camera.hpp>
+#include <string>
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+// This is the main program of your controller.
+// It creates an instance of your Robot instance, launches its
+// function(s) and destroys it at the end of the execution.
+// Note that only one instance of Robot should be created in
+// a controller program.
+// The arguments of the main function can be specified by the
+// "controllerArgs" field of the Robot node
+int main() {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+  Camera *camera = robot->getCamera("camera_sensor");
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+  camera->enable(timeStep);
+  
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  std::string filename("image.jpeg");
+  while (robot->step(timeStep) != -1) {
+    camera->getImage();
+    camera->saveImage(filename, 100);
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+
+    // Enter here functions to send actuator commands, like:
+    //  motor->setPosition(10.0);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
---
+++ controllers/GET_IMAGE/build/release/GET_IMAGE.d
@@ -0,0 +1,6 @@
+build/release/GET_IMAGE.o: GET_IMAGE.cpp \
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Camera.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Device.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/camera_recognition_object.h
