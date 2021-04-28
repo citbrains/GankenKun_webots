f7504cb10a133c58590f70b716e8939ddfa962af
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-04-29 04:13:10 +0900

    remove RTIMULIB directory

--- controllers/hr46_b3m/RTIMULib/CMakeFiles/RTIMULib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for RTIMULib.
-# This may be replaced when dependencies are built.
--- controllers/hr46_b3m/RTIMULib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/satoshi/GankenKun_webots/hr46_b3m/RTIMULib
-# Build directory: /home/satoshi/GankenKun_webots/hr46_b3m/RTIMULib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
