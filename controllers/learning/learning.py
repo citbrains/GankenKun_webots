607bd27484a59f902ae319bd58b4ac419ee7981d
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2021-07-27 23:42:00 +0900

    play motion

--- controllers/learning/learning.py
+++ controllers/learning/learning.py
@@ -31,6 +31,8 @@ import time
 import traceback
 import transforms3d
 import random
+import numpy as np
+import csv
 
 from scipy.spatial import ConvexHull
 
@@ -39,30 +41,44 @@ from types import SimpleNamespace
 # start the webots supervisor
 supervisor = Supervisor()
 time_step = int(supervisor.getBasicTimeStep())
-time_count = 0
 
 field = Field("kid")
 children = supervisor.getRoot().getField('children')
 children.importMFNodeFromString(-1, f'RobocupSoccerField {{ size "kid" }}')
-children.importMFNodeFromString(-1, f'DEF BALL RobocupSoccerBall {{ translation 0 0 0.5 size 1 }}')
-children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.3 0 0.439 rotation 0 0 1 0 controller "GankenKun_controller"}}')
+children.importMFNodeFromString(-1, f'DEF BALL RobocupSoccerBall {{ translation 0 0 0.1 size 1 }}')
+children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.3 0 0.450 rotation 0 0 1 0 controller "play_motion"}}')
 player = supervisor.getFromDef('PLAYER')
+ball = supervisor.getFromDef('BALL')
 player_translation = supervisor.getFromDef('PLAYER').getField('translation')
 player_rotation = supervisor.getFromDef('PLAYER').getField('rotation')
+player_controller = supervisor.getFromDef('PLAYER').getField('controller')
+ball_translation = supervisor.getFromDef('BALL').getField('translation')
+ball_rotation = supervisor.getFromDef('BALL').getField('rotation')
 
 try:
-    previous_real_time = time.time()
-    count = 0
-    while supervisor.step(time_step) != -1:
-        time_count += time_step
-        count += 1
-        if count > 100:
+    for x in np.arange(-0.35, -0.1, 0.01):
+        for y in np.arange(0, 0.25, 0.01):
             count = 0
-            x = -0.8+random.uniform(-0.5,0.5)
-            y = random.uniform(-0.5,0.5)
-            the = random.uniform(-3.14,3.14)
-            player.resetPhysics()
-            player_translation.setSFVec3f([x, y, 0.439])
-            player_rotation.setSFRotation([0, 0, 1, the])
+            player.remove()
+            children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation {x} {y} 0.450 rotation 0 0 1 0 controller "play_motion"}}')
+            player = supervisor.getFromDef('PLAYER')
+            #player.resetPhysics()
+            #player_translation.setSFVec3f([x, y, 0.450])
+            #player_rotation.setSFRotation([0, 0, 1, 0])
+            #player_controller.setSFString("play_motion")
+            ball.resetPhysics()
+            ball_translation.setSFVec3f([0, 0, 0.1])
+            ball_rotation.setSFRotation([0, 0, 1, 0])
+            while supervisor.step(time_step) != -1:
+                count += 1
+                if count > 800:
+                    break
+                if count > 800 - 1:
+                    pos = ball_translation.getSFVec3f()
+                    print(str(x)+", "+str(y)+", "+str(pos[0])+", "+str(pos[1]))
+                    with open('result.csv', 'a', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([x, y, pos[0], pos[1]])
+                    #player_controller.setSFString("void")
 except Exception:
     error(f"Unexpected exception in main referee loop: {traceback.format_exc()}", fatal=True)
