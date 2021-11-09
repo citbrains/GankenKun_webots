ab9d804f3958af4939ad990a7913b52c3aa2f3aa
KANBE8810 <s19c1034bq@s.chibakoudai.jp>
2021-11-09 15:04:18 +0900

    保存機能の追加

--- controllers/test/main.cpp
+++ controllers/test/main.cpp
@@ -2,17 +2,83 @@
 #include <webots/Camera.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <chrono>
+#include <string>
+//#include <experimental/filesystem>
+#include <filesystem>
 
+//namespace fs = std::experimental::filesystem;
+namespace fs = std::filesystem;
 using namespace webots;
 
+class ImageWriter
+{
+public:
+	ImageWriter();
+	~ImageWriter();
+	void createCapturedImageDirectory();
+	void saveCapturedImage(cv::Mat);
+private:
+	bool enable_capture;
+	int image_count;
+	std::chrono::time_point<std::chrono::high_resolution_clock> last_capture_time;
+	std::string capture_save_path;
+};
+
+ImageWriter::ImageWriter() : enable_capture(false), image_count(0)
+{
+	createCapturedImageDirectory();
+	enable_capture = true;
+	last_capture_time = std::chrono::high_resolution_clock::now();
+}
+
+
+ImageWriter::~ImageWriter()
+{
+}
+
+void ImageWriter::createCapturedImageDirectory(void)
+{
+  std::string capture_save_path;
+  std::string capture_dir("game_images");
+  if(!fs::exists(capture_dir)) {
+    fs::create_directory(capture_dir);
+  }
+  std::stringstream ss_capture;
+  auto now_t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  ss_capture << capture_dir << "/" << std::put_time(std::localtime(&now_t), "%Y%m%dT%H%M%S");
+  capture_save_path = ss_capture.str();
+  fs::create_directory(capture_save_path);
+}
+
+void ImageWriter::saveCapturedImage(cv::Mat img)
+{
+  static int image_count = 0;
+  bool enable_capture = true;
+  if(enable_capture) {
+    const auto capture_interval = std::chrono::seconds(1);
+    if(std::chrono::high_resolution_clock::now() - last_capture_time > capture_interval) {
+      std::stringstream ss;
+      ss << capture_save_path << "/" << std::setw(6) << std::setfill('0') << image_count << ".jpg";
+      image_count++;
+      cv::Mat save_img;
+      cv::cvtColor(img, save_img, CV_YCrCb2BGR);
+      cv::imwrite(ss.str().c_str(), save_img);
+      last_capture_time = std::chrono::high_resolution_clock::now();
+    }
+  }
+}
+
 int main() {
+  ImageWriter imagewriter;
   cv::Mat img(480, 640, CV_8UC3);
   Robot *robot = new Robot();
   Camera *camera;
   camera = robot->getCamera("camera_sensor");
   //camera->enable()
   //camera->getImage();
-    camera->enable(13);
+  camera->enable(13);
+  imagewriter.createCapturedImageDirectory();
 
   while(robot->step(32) != -1){
     //mTimeStep = (int)robot->getBasicTimeStep();
@@ -32,6 +98,7 @@ int main() {
     //std::cout << "hello world" << std::endl;
     cv::imshow("test", img);
     cv::waitKey(1);
+    imagewriter.saveCapturedImage(img);
   }
 
     delete robot;
