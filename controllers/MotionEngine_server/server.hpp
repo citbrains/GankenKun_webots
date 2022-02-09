8abcf456965d0086b14aecaf62347fc770fdf0b2
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-02-09 23:23:54 +0900

    finish implementing server

--- controllers/MotionEngine_server/server.hpp
+++ controllers/MotionEngine_server/server.hpp
@@ -1,13 +1,13 @@
 #include "zmq.hpp"
 #include <string>
 #include <iostream>
-#include <future>
 #include <array>
 #include <cstdint>
 #include <cstddef>
-#include <cassert>
 #include <vector>
 #include <utility>
+#include <string_view>
+#include "message.pb.h"
 static const std::string adress("ipc://WebotsMotionEngine");
 
 class MotionEngineCom
@@ -18,23 +18,49 @@ class MotionEngineCom
     }
     /**
      * @brief モーターの角度指令値を受け取る.
-     * 
-     * @return std::vector<std::pair<int32_t, double>> モーター番号とそれを動かす角度指令値[rad] 
+     *
+     * @return std::vector<std::pair<int32_t, double>> モーター番号とそれを動かす角度指令値[rad]
      */
-    std::vector<std::pair<int32_t, double>> getMotorDgrees(){
-
+    std::vector<std::pair<uint32_t, double>> getMotorDgrees()
+    {
+        std::array<std::byte, buf_size_> data;
+        data.fill(static_cast<std::byte>(0));
+        auto buf = zmq::buffer(data.data(), data.size());
+        reply_.recv(buf);
+        webotsMotionEngine::degrees deg;
+        std::string s(reinterpret_cast<const char *>(buf.data()));
+        deg.ParseFromString(s);
+        std::vector<std::pair<uint32_t, double>> return_val;
+        for (size_t i = 0; i < std::min(deg.motor_number_size(), deg.motor_degs_size()); ++i)
+        {
+            return_val.emplace_back(std::make_pair<uint32_t, double>(deg.motor_number(i), deg.motor_degs(i)));
+        }
+        return return_val;
     };
     /**
      * @brief webotsで取得したジャイロと加速度計の値を送信する。こちらを呼び出す前には必ずgetMotorDegreesを呼ぶ.
-     * 
+     *
      * @param gyro x,y,zの3要素 [rad/s]
-     * @param accelerometer x,y,zの3要素 [m/s^2] 
+     * @param accelerometer x,y,zの3要素 [m/s^2]
      */
-    void sendIMUData(const std::vector<double>& gyro,const std::vector<double>& accelerometer){
-
+    void sendIMUData(const std::vector<double> &gyro, const std::vector<double> &accelerometer)
+    {
+        webotsMotionEngine::sensorData sensor;
+        for (const auto &itm : gyro)
+        {
+            sensor.add_gyro(itm);
+        }
+        for (const auto &itm : accelerometer)
+        {
+            sensor.add_accelerometer(itm);
+        }
+        std::string_view data(sensor.SerializeAsString().c_str());
+        auto buf = zmq::buffer(data);
+        reply_.send(buf);
     }
-    private : zmq::context_t ctx_;
-    zmq::socket_t reply_;
-    const size_t buf_size_ = 1024;
-};
 
+private:
+    zmq::context_t ctx_;
+    zmq::socket_t reply_;
+    inline static constexpr size_t buf_size_ = 1024;
+};
