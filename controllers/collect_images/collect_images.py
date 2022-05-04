dc96a5451b277e6e7aa542c3a4f49605b5908c55
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2022-05-04 19:12:40 +0900

    change collect_images

--- controllers/collect_images/collect_images.py
+++ controllers/collect_images/collect_images.py
@@ -1,46 +1,26 @@
-# Copyright 1996-2021 Cyberbotics Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python3
 
-from field import Field
+import os
+import sys
 from controller import Supervisor
 import numpy as np
 
-# start the webots supervisor
+deviceImagePath = os.getcwd()
 supervisor = Supervisor()
-time_step = int(supervisor.getBasicTimeStep())
-
-field = Field("kid")
-children = supervisor.getRoot().getField('children')
-children.importMFNodeFromString(-1, f'RobocupSoccerField {{ size "kid" }}')
-children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.3 0 0.450 rotation 0 0 1 0 controller "capture_image" controllerArgs "x-0.30_y-0.00_the_0.00.jpg"}}')
-player = supervisor.getFromDef('PLAYER')
-player_translation = supervisor.getFromDef('PLAYER').getField('translation')
-player_rotation = supervisor.getFromDef('PLAYER').getField('rotation')
-player_controller = supervisor.getFromDef('PLAYER').getField('controller')
+timestep = int(supervisor.getBasicTimeStep())
+camera = supervisor.getDevice('camera_sensor')
+camera.enable(timestep)
 
 try:
     for x in np.arange(-4.3, 4.3, 0.2):
         for y in np.arange(-3, 3, 0.2):
             for the in np.arange(-1.57, 1.57, 3.14/10):
-                count = 0
-                player.remove()
+                supervisor.getFromDef('PLAYER').getField('translation').setSFVec3f([x, y, 0.450])
+                supervisor.getFromDef('PLAYER').getField('rotation').setSFRotation([0, 0, 1, the])
+                for i in range(1):
+                    supervisor.step(timestep)
                 filename = "x"+format(x,"+.2f")+"_y"+format(y,"+.2f")+"_the"+format(the,"+.3f")+".jpg"
-                children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation {x} {y} 0.450 rotation 0 0 1 {the} controller "capture_image" controllerArgs "{filename}"}}')
-                player = supervisor.getFromDef('PLAYER')
-                while supervisor.step(time_step) != -1:
-                    count += 1
-                    if count > 10:
-                        break
+                camera.saveImage(deviceImagePath + '/images/' + str(filename), 80)
+
 except Exception:
     error(f"Unexpected exception in main referee loop: {traceback.format_exc()}", fatal=True)
