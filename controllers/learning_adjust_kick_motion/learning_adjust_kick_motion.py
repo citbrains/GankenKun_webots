17ed82e4466f3236c336133f758770f95421ab17
hiroki-0001 <touhu.273@gmail.com>
2022-09-22 17:07:25 +0900

    update "learning_adjust_kick_motion"
    モーションの再生を可能にした

--- controllers/learning_adjust_kick_motion/learning_adjust_kick_motion.py
+++ controllers/learning_adjust_kick_motion/learning_adjust_kick_motion.py
@@ -1,5 +1,8 @@
 from controller import Supervisor
 import math
+import csv
+import math
+import sys
 import numpy as np
 
 motor_names = [
@@ -47,6 +50,14 @@ motor_sensor_names = [
     "head_yaw_joint_sensor"                         # ID19
 ]
 
+file_name = "./kick_motion.csv"
+
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+csv_file = open(file_name, "r")
+f = csv.reader(csv_file, delimiter=",", lineterminator="\r\n")
+data = [row for row in f]
+
 supervisor = Supervisor()
 time_step = int(supervisor.getBasicTimeStep())
 
@@ -61,15 +72,45 @@ for i in range (len(motor_sensor_names)):
 accelerometer.enable(time_step)
 gyro.enable(time_step)
 
-while supervisor.step(time_step) != -1:
+while True:
+    t = 0.0   # elapsed simulation time
+    tm = 0.0
+    index = -1
+    angles = [0.0] * len(motors)
+    delta_angles = [0.0] * len(motors)
+
+    while supervisor.step(time_step) != -1:
+        # acc_data = accelerometer.getValues()
+        # gyro_data = gyro.getValues()
+        # print("acc_data = {}".format(acc_data))
+        # print("gyro_data = {}".format(gyro_data))
+        
+        if t >= tm:
+            index += 1
+            if index >= len(data):
+                break
+            try:
+                delta_angles = [(float(data[index][i+1]) - angles[i])/(float(data[index][0])*0.008) for i in range(len(motors))]
+            except ZeroDivisionError:
+                break
+            tm += float(data[index][0]) * 0.01
+        for i in range(len(motors)):
+            angles[i] += delta_angles[i] * 0.01
+            motor_sensor_data = math.degrees(motor_sensors[i].getValue())
+            print("ID = {0} {1} : {2} deg". format(i+1, motors[i], angles[i]))   
+            print("sensor ID = {0} {1} : {2} deg". format(i+1, motor_sensor_names[i], motor_sensor_data))   
+        [motor.setPosition(math.radians(direction[i] * float(angles[i]))) for motor, i in zip(motors, range(len(motors)))]
+        t += time_step / 1000.0
+    break
+
     #acc_data = accelerometer.getValues()
     #gyro_data = gyro.getValues()
     
-    for i in range (len(motor_names)):
-        motors[i].setPosition(math.radians(15))
+    # for i in range (len(motor_names)):
+    #     motors[i].setPosition(math.radians(3))
     
-    for i in range(len(motor_sensor_names)):
-        data = math.degrees(motor_sensors[i].getValue())
-        print("ID = {0} {1} : {2} deg". format(i, motor_sensor_names[i], data))   
+    # for i in range(len(motor_sensor_names)):
+    #     data = math.degrees(motor_sensors[i].getValue())
+    #     print("ID = {0} {1} : {2} deg". format(i+1, motor_sensor_names[i], data))   
     # print("acc_data = {}".format(acc_data))
     # print("gyro_data = {}".format(gyro_data))
