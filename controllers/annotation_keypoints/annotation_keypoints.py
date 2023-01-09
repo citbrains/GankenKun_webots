02890d04957f5d1a82737d7380aa926d9509000f
HirokiHasegawa777 <s21C1094JW@s.chibakoudai.jp>
2023-01-10 04:42:15 +0900

    fix

--- controllers/annotation_keypoints/annotation_keypoints.py
+++ controllers/annotation_keypoints/annotation_keypoints.py
@@ -17,21 +17,21 @@ camera.recognitionEnable(timestep)
 camera.enableRecognitionSegmentation()
 number = 0
 
-print("hasRecognition(): " + str(camera.hasRecognition()))
-print("hasRecognitionSegmentation(): " + str(camera.hasRecognitionSegmentation()))
+#print("hasRecognition(): " + str(camera.hasRecognition()))
+#print("hasRecognitionSegmentation(): " + str(camera.hasRecognitionSegmentation()))
 
 #cv2.startWindowThread()
 #cv2.namedWindow("preview")
 
 color_dict = { \
-#    "ball_color"    :(0.0, 0.0, 1.0), \
+    #"ball_color"    :(0.0, 0.0, 1.0), \
     "colorHead"     :(1.0, 1.0, 1.0), \
 #    "colorNeck"     :(0.5, 0.5, 0.5), \
-    "colorShoulderL":(0.0, 0.5, 0.0), \
-    "colorShoulderR":(0.5, 0.0, 0.0), \
-    "colorElbowL"   :(0.0, 0.6, 0.0), \
-    "colorElbowR"   :(0.6, 0.0, 0.0), \
-    "colorWristL"   :(0.0, 0.7, 0.0), \
+    "colorShoulderL":(0.0, 0.499, 0.0), \
+    "colorShoulderR":(0.0, 0.0, 0.499), \
+    "colorElbowL"   :(0.0, 0.599, 0.0), \
+    "colorElbowR"   :(0.599, 0.0, 0.0), \
+    "colorWristL"   :(0.0, 0.0, 1.0), \
     "colorWristR"   :(0.7, 0.0, 0.0), \
     "colorHipL"     :(0.0, 0.8, 0.0), \
     "colorHipR"     :(0.8, 0.0, 0.0), \
@@ -40,11 +40,10 @@ color_dict = { \
     "colorAnkleL"   :(0.0, 1.0, 0.0), \
     "colorAnkleR"   :(1.0, 0.0, 0.0) \
 }
-pos_dict = dict()
+#pos_dict = dict()
 lines = [
     ["colorHead"     , "colorNeck"     ],
     ["colorNeck"     , "colorShoulderL"],
-    ["colorHead"     , "colorShoulderL"],
     ["colorShoulderL", "colorElbowL"   ],
     ["colorElbowL"   , "colorWristL"   ],
     ["colorNeck"     , "colorShoulderR"],
@@ -60,11 +59,16 @@ lines = [
 
 cnt = -1
 
+memomemo_x = 0
+memomemo_y = 0
+memo_x = 0
+memo_y = 0
+
 while supervisor.step(timestep) != -1:
     # supervisor.getFromDef('BALL').getField('translation').setSFVec3f([random.uniform(0.0, 4.0), random.uniform(-1.0, 1.0), 0.1])
     # supervisor.getFromDef('ENEMY1').getField('rotation').setSFRotation([0, 0, 1, random.uniform(-1.0, 1.0)+3.14])
-    for i in range(10):
-        supervisor.step(timestep)
+#    for i in range(10):
+#        supervisor.step(timestep)
     camera.saveImage(deviceImagePath + '/images/image' + str(number) + '.jpg', 80)
     #camera.saveRecognitionSegmentationImage(deviceImagePath + '/images/segmentation_image' + str(number) + '.jpg', 80)
     number += 1
@@ -74,21 +78,37 @@ while supervisor.step(timestep) != -1:
     cnt +=1
     file_path = 'txt/image' + str(cnt) + '.txt'
 
+
     for key, val in color_dict.items():
         color = (val[0]*255, val[1]*255, val[2]*255, 255)
         area = cv2.inRange(img, color, color)
-        x, y, width, height = cv2.boundingRect(area)
+        x, y, w, h = cv2.boundingRect(area)
         #cv2.rectangle(img, (x, y), (x + width, y + height), color=color, thickness=2)
-        pos_dict[key] = (x + width / 2, y + height / 2)
-        print(key + ": " + str(x) + ", y: " + str(y) + ", with: " + str(width) + ", height: " + str(height))
+        #pos_dict[key] = (x + width / 2, y + height / 2)
+        #print(key + ": " + str(x) + ", y: " + str(y) + ", with: " + str(width) + ", height: " + str(height))
+        #print(key + ": " + str(x) + ", y: " + str(y))
+        x = x + (w-1)/2
+        y = y + (h-1)/2
+        if x == 0 or y ==0: 
+            x = memomemo_x 
+            y = memomemo_y
+            y += 20
+
         with open(file_path, 'a') as f:
-            f.write("%s" % x)
+            f.write("%s" % int(x))
             f.write(',')
-            f.write("%s" % y)
-            if x!=0 or y!=0:
-                f.write(',2\n')
-            else:
-                f.write(',0\n')
+            f.write("%s" % int(y))
+#            if int(x)!=0 or int(y)!=0:
+            f.write(',2\n')
+#            else:
+#                f.write(',0\n')
+        memomemo_x = memo_x
+        memomemo_y = memo_y
+        memo_x = x
+        memo_y = y
+#    if cnt >= 600: 
+#        print("anno_finish\n")
+#        sys.exit()
 
     # for line in lines:
     #     x0, y0 = int(pos_dict[line[0]][0]), int(pos_dict[line[0]][1])
